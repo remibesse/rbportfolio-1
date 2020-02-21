bc6df330cadb70b54a72474f8831563bcae20cb0
Victoire <victoirebaron@hotmail.fr>
2020-02-20 20:07:23 -0800

    Setup packages material UI, router and Helmet

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as HashRouter, Route } from "react-router-dom";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { Helmet } from "react-helmet";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
-function App() {
+const darkTheme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <HashRouter basename="portfolio">
+        <div className="App">
+          <Helmet>
+            <title>Remi Besse</title>
+            <body style="background:#f9f4f3"></body>
+          </Helmet>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+        </div>
+      </HashRouter>
+    </ThemeProvider>
   );
 }
 
-export default App;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+export default function Home() {
+
+    return (
+        <>
+            <Typography>Home page</Typography>
+        </>
+    )
+}
