02d445f779b831fff823f7f37377d5d40c571df6
Victoire Baron <victoirebaron@hotmail.fr>
2020-03-14 18:09:48 -0700

    title animation

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { makeStyles } from "@material-ui/core";
 import { Typography } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
     root: {
         "& .MuiTypography-root": {
-            position: "fixed",
-            left: "35%",
+            position: "absolute",
             zIndex: "9990",
+            width: "100%",
             fontSize: theme.spacing(4),
             textTransform: "uppercase",
             fontWeight: "bold",
@@ -23,11 +23,21 @@ const useStyles = makeStyles(theme => ({
 
 export default function Title() {
     const classes = useStyles()
-    // const texts = ["img", "entertainer", "hustler"];
+    const [index, setIndex] = useState(0)
+    const wordsArray = ["moviemak", "imgmak", "entertain", "hustl"];
+    const changeWordTimeout = useRef()
+
+    useEffect(() => {
+        changeWordTimeout.current = setTimeout(
+            () => setIndex((index + 1) % wordsArray.length),
+            3000
+        )
+        return () => clearTimeout(changeWordTimeout.current)
+    });
 
     return (
         <div className={classes.root}>
-            <Typography>Your favorite moviemaker</Typography>
+            <Typography>Your favorite <span>{wordsArray[index]}</span>er</Typography>
         </div>
     )
 }
